6083d3c13c93367b139424f68dc0e30f0a6bae1f
Moshe Berman <moshberm@gmail.com>
2014-12-07 13:39:51 -0500

    Added a new tokens class to the project.

---
+++ Yum/YumTokens.swift
@@ -0,0 +1,24 @@
+//
+//  YumTokens.swift
+//  Yum
+//
+//  Created by Moshe on 12/7/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+import Cocoa
+
+class YumTokens: NSObject {
+
+    class var clientToken : NSString {
+        return ""
+    }
+    
+    class var appID : NSString {
+        return ""
+    }
+    
+    class var appSecret : NSString {
+        return ""
+    }
+}
